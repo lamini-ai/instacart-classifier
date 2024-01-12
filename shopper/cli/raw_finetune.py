517db743a5e29712061469cff173e0ac194fd201
Sharon Zhou <sharon@lamini.ai>
2024-01-12 11:24:55 -0800

    eval

--- shopper/cli/raw_finetune.py
+++ shopper/cli/raw_finetune.py
@@ -1,4 +1,4 @@
-from lamini import Lamini
+from lamini import Lamini, MistralRunner
 import lamini
 
 from tqdm import tqdm
@@ -10,7 +10,7 @@ import jsonlines
 logger = logging.getLogger(__name__)
 
 
-def main():
+def train():
     """Train an LLM on raw csv of products and their info."""
 
     parser = argparse.ArgumentParser(
@@ -51,8 +51,36 @@ def main():
             }
             training_data.append(product)
     print(training_data)
+
+    # Train without prompt template
     llm = Lamini(model_name="mistralai/Mistral-7B-Instruct-v0.1")
     llm.train(data=training_data)
 
-main()
+def run():
+    trained_1k_name = "8298a5118fbbb0a62430a023b8f7854a9f08fd6b8f71980bd58c362a2eb7a951"
 
+    eval_questions = [
+        "I'm planning a romantic dinner for my anniversary, what should I add to my cart?",
+        "What kind of snacks should I get for a kids' birthday party?",
+        "Can you suggest some vegan options for a plant-based picnic?",
+        "I'm grilling this weekend, what sides would complement BBQ ribs?",
+        "What are some essential ingredients for a traditional Thanksgiving dinner?",
+    ]
+
+    # Without prompt template
+    llm_without_template = Lamini(model_name=trained_1k_name)
+
+    # With prompt template
+    llm_with_template = MistralRunner(model_name=trained_1k_name)
+
+    for question in eval_questions:
+        print("===============Question===============")
+        print(question)
+        print("===============Without prompt template===============")
+        print(llm_without_template.generate(question + " " + "Use Instacart products in your recommendations and include their product IDs."))
+        print("===============With prompt template===============")
+        print(llm_with_template.call(question, system_prompt="You are a grocery product expert for Instacart. Include Instacart products in each of your recommendations, including the product's product ID."))
+        print()
+
+# train()
+run()
